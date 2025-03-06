bb8d4056bc674e60c41c5463afaad356cf623dc2
youngplq10 <patryk.plq@o2.pl>
2025-03-06 15:55:20 +0100

    Changed navigation to show nav depends on user isauth status

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,11 +1,25 @@
-import { Stack, Typography } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 import Link from 'next/link'
 import React from 'react'
+import Loading from './Loading'
 
-const Navigation = () => {
+const Navigation = ({ isAuth, isLoading } : { isAuth: boolean, isLoading: boolean }) => {
     return (
         <Stack direction="row" spacing={2}>
-            <Link href='/sign-in' className='text-decoration-none'><Typography variant='h4' color='textPrimary'>Sign in</Typography></Link>
+            { isLoading ? (
+                <Box sx={{ height: 30, width: 200 }}>
+                    <Loading />
+                </Box>
+            ) : (
+                isAuth ? (
+                    <>
+                        <Link href='/dashboard' className='text-decoration-none'><Typography variant='h4' color='textPrimary'>Dashboard</Typography></Link>
+                        <Link href='/log-out' className='text-decoration-none'><Typography variant='h4' color='textPrimary'>Logout</Typography></Link>
+                    </>
+                ) : (
+                    <Link href='/sign-in' className='text-decoration-none'><Typography variant='h4' color='textPrimary'>Sign in</Typography></Link>
+                )
+            ) }
         </Stack>
     )
 }
--- src/app/modules/Navbar.tsx
+++ src/app/modules/Navbar.tsx
@@ -1,17 +1,32 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
 import Logo from '../components/Logo'
 import Navigation from '../components/Navigation'
+import { getIsAuthenticated } from '../scripts/server'
 
 const Navbar = () => {
+    const [isLogged, setIsLogged] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchIsAuth = async () => {
+            const isAuth = await getIsAuthenticated();
+            setIsLogged(isAuth);
+            setIsLoading(false);
+        }
+        fetchIsAuth();
+    }, [])
+
     return (
         <div className='container-lg'>
             <div className='row my-3'>
-                <div className="col-6">
+                <div className="col-auto">
                     <Logo />
                 </div>
 
                 <div className="col-auto ms-auto">
-                    <Navigation />
+                    <Navigation isAuth={isLogged} isLoading={isLoading} />
                 </div>
             </div>
         </div>
