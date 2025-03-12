65c2a83928d73077bcd663e6ce0fdb68e07c7398
youngplq10 <patryk.plq@o2.pl>
2025-03-12 19:56:41 +0100

    Created payment failed page

---
+++ src/app/dashboard/credits/cancel/page.tsx
@@ -0,0 +1,14 @@
+import CancelPaymentPage from '@/app/modules/CancelPaymentPage'
+import Navbar from '@/app/modules/Navbar'
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <Navbar />
+            <CancelPaymentPage />
+        </>
+    )
+}
+
+export default page
---
+++ src/app/modules/CancelPaymentPage.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const CancelPaymentPage = () => {
+    return (
+        <div className='container-lg my-3'>
+            <Typography variant='h4'>Payment failed.</Typography>
+        </div>
+    )
+}
+
+export default CancelPaymentPage
